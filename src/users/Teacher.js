585449b9f34cba5f11485667623c97de6bfc9d82
Anwar Montasir <anwarmontasir@gmail.com>
2017-09-19 11:22:05 -0700

    logout button works, begin adding profile constants

--- src/users/Teacher.js
+++ src/users/Teacher.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../app/Header';
+import Profile from './components/Profile';
 import { connect } from 'react-redux';
 
 export default function Teacher() {
