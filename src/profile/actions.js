1a444a64758dd701f7bd999736640aa34cc38aef
Anwar Montasir <anwarmontasir@gmail.com>
2017-09-19 11:54:50 -0700

    created actions and profileApi

---
+++ src/profile/actions.js
@@ -0,0 +1,44 @@
+import * as actions from './constants';
+import api from '../services/profileApi';
+
+export function getProfile(status) {
+  return dispatch => {
+    dispatch({ type: actions.FETCHING_PROFILE });
+
+    api.get(status)
+      .then(profile => {
+        dispatch({ type: actions.FETCHED_PROFILE, payload: profile });
+      })
+      .catch(error => {
+        dispatch({ type: actions.FETCHED_PROFILE_ERROR, payload: error });
+      });
+  };
+}
+
+export function updateImage(status, data) {
+  return dispatch => {
+    dispatch({ type: actions.UPDATING_IMAGE });
+
+    api.update(status, data)
+      .then(updated => {
+        dispatch({ type: actions.UPDATED_IMAGE, payload: updated });
+      })
+      .catch(error => {
+        dispatch({ type: actions.UPDATED_IMAGE_ERROR, payload: error });
+      });
+  };
+}
+
+export function updateText(status, data) {
+  return dispatch => {
+    dispatch({ type: actions.UPDATING_TEXT });
+  
+    api.update(status, data)
+      .then(updated => {
+        dispatch({ type: actions.UPDATED_TEXT, payload: updated });
+      })
+      .catch(error => {
+        dispatch({ type: actions.UPDATED_TEXT_ERROR, payload: error });
+      });
+  };
+}
