c6628f15fb4b1a2d774695df73027832fcbff7f2
Haley Tortorella <hdtort@gmail.com>
2017-09-21 16:15:54 -0700

    Courses actions created

---
+++ src/courses/actions.js
@@ -0,0 +1,47 @@
+import * as actions from './constants';
+import api from '../services/coursesApi';
+
+export function getCourses() {
+
+  return dispatch => {
+    dispatch({ type: actions.FETCHING_COURSES });
+
+    api.get()
+      .then(courses => {
+        dispatch({ type: actions.FETCHED_COURSES, payload: courses });
+      })
+      .catch(error => {
+        dispatch({ type: actions.FETCHED_COURSES_ERROR, payload: error });
+      });
+  };
+}
+
+export function addClass(course) {
+
+  return dispatch => {
+    dispatch({ type: actions.ADDING_COURSE });
+
+    api.add(course)
+      .then(saved => {
+        dispatch({ type: actions.ADDED_COURSE, payload: saved });
+      })
+      .catch(error => {
+        dispatch({ type: actions.ADDED_COURSE_ERROR, payload: error });
+      });
+  };
+}
+
+export function removeCourse(courseId) {
+
+  return dispatch => {
+    dispatch({ type: actions.REMOVING_COURSE });
+
+    api.remove(courseId)
+      .then(() => {
+        dispatch({ type: actions.REMOVED_COURSE, payload: courseId });
+      })
+      .catch(error => {
+        dispatch({ type: actions.REMOVED_COURSE_ERROR, payload: error });
+      });
+  };
+}
