1bb3bb5cd4af771dc65971105eed9d4ee0de3db0
Anwar Montasir <anwarmontasir@gmail.com>
2017-09-21 14:14:20 -0700

    intro page grey style exploration

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Intro from './Intro';
+
+const Welcome = () => (
+  <div id="welcome">
+    <div id="logo"><i className="fa fa-star fa-5x"></i></div>
+    <h1>GoldStar</h1>
+    <p>An Educational Flash Card App</p>
+  </div>
+);
 
 const GetStarted = () => (
-  <div>
-    <p>Click to get started as a</p>
-    <p>
-      <Link to="/authTeacher/signin">Teacher</Link>
-      {' or '} 
-      <Link to="/authStudent/signin">Student</Link>
-    </p>
+  <div id="getStarted">
+    <Link to="/authTeacher/signin">
+      <div className="getStartedBtn">
+        <span className="getStarted-small">get started as a</span>
+        <span className="getStarted-large">Teacher</span>
+      </div>
+    </Link>
+    <Link to="/authStudent/signin">
+      <div className="getStartedBtn">
+        <span className="getStarted-small">get started as a</span>
+        <span className="getStarted-large">Student</span>
+      </div>
+    </Link>
   </div>
 );
 
 function Home() {
   return(
     <div>
-      <GetStarted />
+      <div id="homeWrapper">
+        <Welcome />
+        <GetStarted />
+      </div>
+      <div id="introWrapper">
+        <Intro />
+      </div>
     </div>
   );
 }
