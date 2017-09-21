23ff0108183c071dc1c7d09081306656f8721b7d
Anwar Montasir <anwarmontasir@gmail.com>
2017-09-21 16:27:48 -0700

    begin styling search box

--- src/search/Search.js
+++ src/search/Search.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 export function Search({ onSearch }) {
   return (
-    <form onSubmit={e => {
-      e.preventDefault();
-      const { search } = e.target.elements;
-      onSearch(search.value);   
-    }}>
-      <input name="search" />
-      <button type="submit"> Search </button>
-    </form>
+    <div id="flashcardSearch">
+      <h2>Looking for flash cards?</h2>
+      <form onSubmit={e => {
+        e.preventDefault();
+        const { search } = e.target.elements;
+        onSearch(search.value);   
+      }}>
+        <input name="search" placeholder="Search your topic here." />
+        <button type="submit"> Search </button>
+      </form>
+    </div>
   );
 }
