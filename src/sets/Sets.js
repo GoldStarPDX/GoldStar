fff8228b260393ae2d74e9896c60e0c6009beb9b
Meryl Turner <merylturner08@gmail.com>
2017-09-21 12:11:26 -0700

    started on AddSet

--- src/sets/Sets.js
+++ src/sets/Sets.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getAllSets, addSet, updateSet, deleteSet } from './actions';
+import AddSet from './AddSet';
 
 export class Sets extends Component {
   componentWillMount() {
@@ -12,15 +13,23 @@ export class Sets extends Component {
   }
 
   render() {
+    const { userSets } = this.props;
+    const setsLength = userSets.length;
+    let setMessage = '';
+    if (setsLength === 0) {
+      setMessage = 'You don\'t have any sets.';
+    }
     return (
       <div>
         <h2>Your Flashcard Sets</h2>
-        {this.props.userSets.map(userSet => {
+        {setMessage}
+        {userSets.map(userSet => {
           return <div key={userSet.id}>
             <h3>{userSet.name}</h3>
             <p>Number of Cards: {userSet.cards.length}</p>
           </div>;
         })}
+        <AddSet onAdd={({set}) => addSet(set)}/>
       </div>
     );
   }
