2ff5d8df606275433a37703aa8a61a54f41fced6
Meryl Turner <merylturner08@gmail.com>
2017-09-22 11:28:33 -0700

    gallery works!!!

--- src/view/Gallery.js
+++ src/view/Gallery.js
@@ -4,8 +4,8 @@ export default class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: 'term'
-
+      show: 'term',
+      index: 0
     };
     this.handleShowChange.bind(this);
   }
@@ -13,27 +13,30 @@ export default class List extends Component {
   handleShowChange = (side, view) => {
     this.setState({ show: side, view: view });
   }
-  
+
 
   render() {
     const { cards, onDelete } = this.props;
-    console.log(cards);
+    const currentCard = cards[this.state.index];
     let side = null;
+    if (this.state.show === 'term') side = <div onClick={() => this.handleShowChange('definition')}> {currentCard.term} </div>;
+    else side = <div onClick={() => this.handleShowChange('term')}> {currentCard.definition} </div>;
 
-    {if(this.state.show === 'term') side = <div onClick={() => this.handleShowChange('definition')}> {cards[0].term} </div>;
-      else side = <div onClick={() => this.handleShowChange('term')}> {cards[0].definition} </div>;} 
     return (
       <ul className="list">
         <p>MY GALLERY</p>
-        {cards.map(card => {
+        <button disabled={this.state.index === 0} onClick={() => this.setState({ index: this.state.index - 1 })}>Previous</button>
+        {/* {currentCard.term}
+        {currentCard.definition} */}
+        {/* {cards.map(card => {
+          if(this.state.show === 'term') side = <div onClick={() => this.handleShowChange('definition')}> {card.term} </div>;
+          else side = <div onClick={() => this.handleShowChange('term')}> {card.definition} </div>;
           return <div key={card._id}>
-            {/* {side} */}
-            <div onClick={() => this.handleShowChange(this.state.show)}> TERM:{card.term} </div>;
-            <div onClick={() => this.handleShowChange(this.state.show)}> DEFINITION: {card.definition} </div>;
-            }
-            <button onClick={() => onDelete(card._id)}>delete</button>
           </div>;
-        })}
+        })} */}
+        {side}
+        <button onClick={() => onDelete(currentCard._id)}>delete</button>
+        <button disabled={this.state.index === cards.length - 1} onClick={() => this.setState({ index: this.state.index + 1 })}>Next</button>
       </ul>
     );
   }
